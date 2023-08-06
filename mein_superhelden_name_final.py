14f91a30b5e86df3a1418e695c84c03fe187d4c8
Sumselbrumm <141552474+Sumselbrumm@users.noreply.github.com>
2023-08-06 11:39:55 +0200

    initial

---
+++ mein_superhelden_name_final.py
@@ -0,0 +1,24 @@
+
+#Superhelden Name aus 3 Ziffern je 1-5 
+characteristics = ["Grimmiger", "Garstiger", "Wuselnder","Mächtiger", "Überdimensionaler", "Oberaffengeiler"]
+nouns = ["Sternen", "Melonen", "Fels", "Hut", "Gott"]
+executions =["Zermatscher", "Zerstörer", "Vernichter", "Verzwirbler", "Zerfetzer"]
+
+#Funktionen definieren
+def input_number(text, list):
+    component = int(input(text))
+    if component in range(1,len(list)+1,1):
+        pass
+    else:
+        component = input_number("Deine Zahl muss 1, 2, 3, 4, oder 5, sein. Bitte neu wählen:")
+    return component
+
+#user input
+first_component = input_number("Gib hier deine erste Zahl zwischen 1 und " + str(len(characteristics)) + " ein: ", characteristics)
+second_component = input_number("Gib hier deine zweite Zahl zwischen 1 und " + str(len(nouns)) + " ein: ", nouns)
+third_component = input_number("Gib hier deine dritte Zahl zwischen 1 und " + str(len(executions)) + " ein: ", executions)
+    
+print()
+print("Dein Superheldenname ist: ", characteristics[first_component-1], nouns[second_component-1], executions[third_component-1])
+print()
+
